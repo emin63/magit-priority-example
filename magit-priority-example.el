2890ca56971ede55bc85e93f429a200b8bcf38fc
Emin Martinian <emin@GHOST-LEGION.localdomain>
2022-04-12 12:33:18 -0400

    Added forge-list-orgit-labeled-issues-table and friends

--- magit-priority-example.el
+++ magit-priority-example.el
@@ -24,8 +24,13 @@
 ;; apply labels like 'priority:1' and 'priority:2' then you can
 ;; call forge-list-labeled-issues with LABEL as 'priority:%' to get
 ;; prioritized issues.
+;;
+;; As a fancier example, see the forge-list-orgit-labeled-issues-table
+;; to get a sorted list of labeled issues as an org-table.
 
 ;;; Code:
+
+(require 'forge-list)
 
 (defun forge-list-labeled-issues (id label)
   "List issues of the repo with given ID that have LABEL.
@@ -106,7 +111,7 @@ the label into the issue title."
    )
   )
 
-  
+
 (defun forge-list-labeled-issues-main (id label)
   "List issues of the repo with given ID that have LABEL.
 
@@ -144,6 +149,155 @@ now to display the label."
     )
   )
 
+(defun forge-list-labeled-issues-multi-query (id-list label)
+  "Query multiple ID values for LABEL in ID-LIST and combine.
+
+This is like forge-list-labeled-issues-query but combines
+together lists from multiple repos.  The problem is that this does
+not quite work the way you might want because the
+
+  forge-list-labeled-issues-main
+
+function calls the macro
+
+  #'forge-issue-list-mode
+
+which actually wants an ID not a list.  Partly it wants an ID not
+a list because it wants to be able to know what repo an issue is
+from in case you get multiple issues with the same ID."
+  (let ((full-list nil))
+    (dolist (my-id id-list full-list)
+      (setq full-list
+	    (append full-list
+		    (forge-list-labeled-issues-query my-id "priority:%")
+		    )
+	    )
+      )
+    )
+  )
+
+(defun forge-list-labeled-issues-pquery (id label)
+  "Return property list of data for issues from repo with ID and LABEL.
+
+This queries a repository with the given ID for a repo with the
+given LABEL.  You can obtain this ID using something
+like `(slot-value (forge-get-repository t) 'id)`.
+
+For example, if you use a label of `\"priority:%\"` then you will
+get all labels which look like a priority setting."
+  (forge-sql
+   [:select
+    [ 'id issue:id
+	  'label label:name
+	  'title issue:title
+	  'number issue:number
+	  'repo repository:name
+    ]
+   :from [issue issue_label label repository] :where
+   (and
+    (= issue:repository repository:id) ;; join issue to repository
+    (= issue_label:issue issue:id)
+    (= issue_label:id    label:id)
+    (= issue:repository  $s1) ; $s1 means first thing in params below
+    (like label:name     $s2) ; $s2 means second thing in params below
+    (isnull issue:closed)
+    )
+   :order-by [(asc label:name updated)]
+   ]
+   id label ;; params list referenced by query
+   )
+  )
+
+
+(defun forge-orgit-clean-string (my-str ttrim &optional ender)
+  "Clean a string MY-STR by trimming it if longer than TTRIM and adding ENDER.
+
+If MY-STR is too long, we trim it to length TTRIM and add the ending ENDER."
+  (if ttrim
+      (let ((slen (length my-str)))
+	(if (> slen ttrim)
+	    (concat
+	     (substring my-str 0 ttrim)
+	     (or ender "")
+	     )
+	  my-str
+	  )
+	)
+    my-str
+    )
+  )
+
+    
+(defun forge-orgit-format-labeled-issue
+    (issue-plist &optional ttrim ender)
+  "Convert ISSUE-PLIST into string org-table row.
+
+This takes as input an ISSUE-PLIST as produced by
+forge-list-labeled-issues-pquery and converts it into an
+org-table row.  This is meant to be called by the
+forge-list-orgit-labeled-issues-table function.
+
+Optionally you can provide TTRIM as a length to trim the title to
+and ENDER as something to append when the title is
+trimmed (e.g., \"..\")."
+  (format "| [[orgit-topic:%s][%s#%s]] | %s | %s |"
+	  (plist-get issue-plist 'id)
+	  (plist-get issue-plist 'repo)
+	  (plist-get issue-plist 'number)
+	  (forge-orgit-clean-string
+	   (plist-get issue-plist 'title) ttrim ender)
+	  (plist-get issue-plist 'label)
+	  )
+  )
+
+(defun forge-list-orgit-labeled-issues-table
+    (id-list label &optional do-insert ttrim ender)
+  "Convert ID-LIST of magit repos using LABEL into an org-table string.
+
+The ID-LIST should be a list of orgit-forge repos.  You can obtain
+this ID using something like `(slot-value (forge-get-repository t) 'id)`
+and then make a list of those IDs.  The LABEL should be a SQL pattern
+such as \"priority:%\" for the label you want to query.
+
+The result is a string representing an org-table (sorted by
+label) showing the matching issues.
+
+If you provide the optional DO-INSERT, then we insert the result
+into the current buffer in addition to providing it as a return
+value.
+
+The TTRIM and ENDER are passed down to forge
+forge-orgit-format-labeled-issue.  See there for
+details.  Suggested values are 45 and \"..\"."
+  (let ((result nil))
+    (dolist (my-id id-list)
+      (setq result
+	    (append result (forge-list-labeled-issues-pquery
+			    my-id label)))
+      )
+    (setq result
+	  (sort result
+		(lambda (first second)
+		  (string-lessp
+		   (plist-get first 'label)
+		   (plist-get second 'label)
+		   )
+		  )
+		)
+	  )
+    (setq result
+	  (concat "| Link | Title | Label |\n"
+		  (mapconcat
+		   (lambda (item)
+		     (forge-orgit-format-labeled-issue
+		      item ttrim ender)
+		     ) result "\n")
+		  )
+	  )
+    (if do-insert (insert result))
+    result
+    )
+  )
 
 ;;; _
 (provide 'magit-priority-example)
